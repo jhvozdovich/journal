66cbe1917a331fc4990346139c896babd5146a7d
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-13 13:21:53 -0700

    update file directory layouts

---
+++ src/journal.js
@@ -0,0 +1,1 @@
+// Backend Buisness Logic
--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@
 // import { vowels } from "./vowels.js";
 // import { consonants } from "./consonants.js";
 // import { teaser } from "./teaser.js";
-// import "./styles.css";
+import "./styles.css";
+import "./jquery-3.4.1.js";
+import "./bootstrap.css";
 
 function journal(title, body) {
   $("#entries").html("<h3>" + title + "</h3>");
   $("#entries").html("<br>");
   $("#entries").html("<p>" + body + "</p>");
 }
-
 
 $(document).ready(function() {
   $("#journal-form").submit(function(event) {
