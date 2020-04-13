9c9c3f9be34b2fd959842d6e00b4ef953482e816
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-13 11:46:39 -0700

    pull outputs for the journal entries

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
-import { journal } from "./journal.js";
-import { vowels } from "./vowels.js";
-import { consonants } from "./consonants.js";
-import { teaser } from "./teaser.js";
-import "./styles.css";
+// Frontend UI Logic
+
+// import { journal } from "./journal.js";
+// import { vowels } from "./vowels.js";
+// import { consonants } from "./consonants.js";
+// import { teaser } from "./teaser.js";
+// import "./styles.css";
+
+function journal(title, body) {
+  $("#entries").html("<h3>" + title + "</h3>");
+  $("#entries").html("<br>");
+  $("#entries").html("<p>" + body + "</p>");
+}
+
 
 $(document).ready(function() {
   $("#journal-form").submit(function(event) {
     event.preventDefault();
     var title = $("#title").val();
     var body = $("#body").val();
-
-  })
-})
+    
+    journal(title, body);
+  });
+});
